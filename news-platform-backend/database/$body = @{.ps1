089f70a177f72e439a747eed9c5be84bc1e42b92
Aly Yehia <    GitHub>
2026-09-21 08:47:06 +0300

    Update

--- news-platform-backend/database/$body = @{.ps1
+++
@@ -1,7 +0,0 @@
-$body = @{
-    full_name = "Alaa Yehia"
-    email = "alaa@example.com"
-    password = "SecurePassword123"
-} | ConvertTo-Json
-
-Invoke-RestMethod -Uri "http://localhost:3000/api/v1/auth/register" -Method Post -Body $body -ContentType "application/json"
